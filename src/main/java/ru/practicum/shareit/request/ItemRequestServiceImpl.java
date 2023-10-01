e0d37b8aa2ef75db6ee4576324a549a7ac6e677b
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-10-01 15:20:15 +0300

    change test exception

--- src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -9,21 +9,22 @@ import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestMapper;
 import ru.practicum.shareit.request.dto.ItemRequestResponseDto;
 import ru.practicum.shareit.user.User;
-import ru.practicum.shareit.user.UserRepository;
+import ru.practicum.shareit.user.UserService;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class ItemRequestServiceImpl implements ItemRequestService {
     private final ItemRequestRepository itemRequestRepository;
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final ItemRequestMapper itemRequestMapper;
 
     @Override
     public ItemRequestResponseDto create(ItemRequestDto itemRequestDto, long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NotFoundException(String.format("user %s not found", userId)));
+        User user = userService.getUserById(userId);
         ItemRequest itemRequest = itemRequestRepository.save(itemRequestMapper.toItemRequestItem(itemRequestDto, user));
         return itemRequestMapper.toItemRequestDto(itemRequest);
 
@@ -31,24 +32,25 @@ public class ItemRequestServiceImpl implements ItemRequestService {
 
     @Override
     public Collection<ItemRequestResponseDto> findByUserId(long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NotFoundException(String.format("user %s not found", userId)));
+        User user = userService.getUserById(userId);
         Collection<ItemRequest> itemRequests = itemRequestRepository.findAllByRequester(user);
         return itemRequestMapper.toItemRequestDtos(itemRequests);
     }
 
     @Override
     public Collection<ItemRequestResponseDto> findAll(int from, int size, long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NotFoundException(String.format("user %s not found", userId)));
+        User user = userService.getUserById(userId);
         PageRequest pageRequest = PageRequest.of(from / size, size);
-        return itemRequestMapper.toItemRequestDtos(itemRequestRepository.findAllByRequesterIsNot(user, pageRequest));
+        return itemRequestMapper.toItemRequestDtos(
+                itemRequestRepository.findAllByRequesterIsNot(user, pageRequest)
+                        .stream()
+                        .sorted(Comparator.comparing(ItemRequest::getCreated).reversed())
+                        .collect(Collectors.toList()));
     }
 
     @Override
     public ItemRequestResponseDto findById(long requestId, long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NotFoundException(String.format("user %s not found", userId)));
+        userService.getUserById(userId);
         ItemRequest itemRequest = itemRequestRepository
                 .findById(requestId)
                 .orElseThrow(() -> new NotFoundException(String.format("itemRequest %s not found", requestId)));
