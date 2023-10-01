0cd63a3a6c58b5765d83747442169acfa1345041
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-10-01 15:59:45 +0300

    fix chef Remarks

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name  varchar(100),
-    email varchar(255),
+    name  varchar(100) NOT NULL,
+    email varchar(255) NOT NULL,
     CONSTRAINT UQ_USER_EMAIL UNIQUE (email)
 );
 
 CREATE TABLE IF NOT EXISTS item_requests
 (
     id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    description VARCHAR(255),
+    description VARCHAR(255)                NOT NULL,
     user_id     BIGINT REFERENCES users (id),
     created     TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
@@ -17,8 +17,8 @@ CREATE TABLE IF NOT EXISTS item_requests
 CREATE TABLE IF NOT EXISTS items
 (
     id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name        varchar(100),
-    description varchar(255),
+    name        varchar(100) NOT NULL,
+    description varchar(255) NOT NULL,
     available   boolean,
     user_id     BIGINT references users (id),
     request_id  BIGINT references item_requests (id)
@@ -31,13 +31,13 @@ CREATE TABLE IF NOT EXISTS bookings
     end_date   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     item_id    BIGINT references items (id),
     user_id    BIGINT references users (id),
-    status     varchar(100)
+    status     varchar(100)                NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS comments
 (
     id      BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    text    VARCHAR2(255),
+    text    VARCHAR2(255)               NOT NULL,
     item_id BIGINT references items (id),
     user_id BIGINT references users (id),
     created TIMESTAMP WITHOUT TIME ZONE NOT NULL
