4c6d1d27b3041e7aec193e0997ecce5f9bb4fb8c
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-10-08 20:10:16 +0300

    fix remarks

--- server/src/main/java/ru/practicum/shareit/booking/BookingStateByBooker/BookingStateFetchByBookerStrategyCurrent.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingStateByBooker/BookingStateFetchByBookerStrategyCurrent.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+import ru.practicum.shareit.booking.BookingStatusFilter;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.repository.BookingRepository;
-import ru.practicum.shareit.booking.BookingStatusFilter;
 import ru.practicum.shareit.user.User;
 
 import java.util.Collection;
@@ -17,10 +17,12 @@ import static ru.practicum.shareit.config.DataUtilsService.now;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class BookingStateFetchByBookerStrategyCurrent implements BookingStateFetchByBookerStrategy {
     private final BookingRepository bookingRepository;
+
     @Override
     public BookingStatusFilter getStrategyName() {
         return BookingStatusFilter.CURRENT;
     }
+
     @Override
     public Collection<Booking> fetch(User user, Pageable pageable) {
         return bookingRepository.findBookingsByBookerAndStartBeforeAndEndAfter(user, now(), now(), pageable);
