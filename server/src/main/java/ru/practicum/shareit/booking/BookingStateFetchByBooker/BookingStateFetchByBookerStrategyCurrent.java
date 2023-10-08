c0ea425cf398c7ff93568e172b9f0142b671c21e
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-10-08 20:26:46 +0300

    fix remarks

--- server/src/main/java/ru/practicum/shareit/booking/BookingStateFetchByBooker/BookingStateFetchByBookerStrategyCurrent.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingStateFetchByBooker/BookingStateFetchByBookerStrategyCurrent.java
@@ -4,10 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.booking.Booking;
-import ru.practicum.shareit.booking.BookingRepository;
+import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.BookingStatusFilter;
-import ru.practicum.shareit.config.DataUtilsService;
+import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.user.User;
 
 import java.util.Collection;
